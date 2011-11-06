188ac1d4aef5be54e6eb2d753642c3e83d61c33c
cenan ozen <cenan.ozen@gmail.com>
2011-11-06 22:28:39 +0200

    'serve_dir' configuration option to specify which directory to serve on the dev server

--- dev_server.py
+++ dev_server.py
@@ -9,7 +9,8 @@ class SsgDevServer(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		try:
-			f = open(os.path.join(config.get("output_dir"), self.path[1:]), "rb")
+			serve_dir = config.get("serve_dir", config.get("output_dir"))
+			f = open(os.path.join(serve_dir, self.path[1:]), "rb")
 			self.send_response(200)
 			filetype, _ = mimetypes.guess_type(self.path)
 			self.send_header('Content-type', filetype)
