692a3f42c5549754297a0720437cb6b7d512c374
cenan ozen <cenan.ozen@gmail.com>
2011-11-06 17:57:58 +0200

    implemented a basic development server

---
+++ dev_server.py
@@ -0,0 +1,20 @@
+import os
+from BaseHTTPServer import HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+import mimetypes
+
+
+class SsgDevServer(BaseHTTPRequestHandler):
+
+	def do_GET(self):
+		try:
+			f = open(os.path.join(os.getcwd(), self.path[1:]), "rb")
+			self.send_response(200)
+			filetype, _ = mimetypes.guess_type(self.path)
+			self.send_header('Content-type', filetype)
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+		except IOError:
+			self.send_error(404, "File not found: %s" % self.path)
+
